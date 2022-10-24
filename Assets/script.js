033b4e73ad102335cee253deac511a4d3d217567
mdh1080 <110183347+mdh1080@users.noreply.github.com>
2022-10-24 15:07:34 -0400

    Add js for 5 day forecast. Edit html for 5 day forecast. Edit css.

--- Assets/script.js
+++ Assets/script.js
@@ -99,15 +99,17 @@ function setList(results) {
                             console.log(response.status);
                             return response.json();
                         })
+
+                    //Five day forecast
                         .then(function (data) {
-                            console.log(data);
+                            console.log(data)
                             let day0dt=document.querySelector("#col-0")
                             let day0Temp = document.querySelector("#day0-temp")
                             let day0Wind = document.querySelector("#day0-wind")
                             let day0Humidity = document.querySelector("#day0-humidity")
                             day0dt.textContent=data.list[0].dt_txt
                             day0Temp.textContent = data.list[0].main.temp
-                            day0Wind.textContent = data.list[0].wind.speed
+                            day0Wind.textContent = data.list[0].weather[0].id
                             day0Humidity.textContent= data.list[0].main.humidity
                             let day1dt=document.querySelector("#col-1")
                             let day1Temp = document.querySelector("#day1-temp")
@@ -117,9 +119,34 @@ function setList(results) {
                             day1Temp.textContent = data.list[8].main.temp
                             day1Wind.textContent = data.list[8].wind.speed
                             day1Humidity.textContent= data.list[8].main.humidity
+                            let day2dt=document.querySelector("#col-2")
+                            let day2Temp = document.querySelector("#day2-temp")
+                            let day2Wind = document.querySelector("#day2-wind")
+                            let day2Humidity = document.querySelector("#day2-humidity")
+                            day2dt.textContent=data.list[16].dt_txt
+                            day2Temp.textContent = data.list[16].main.temp
+                            day2Wind.textContent = data.list[16].wind.speed
+                            day2Humidity.textContent= data.list[16].main.humidity
+                            let day3dt=document.querySelector("#col-3")
+                            let day3Temp = document.querySelector("#day3-temp")
+                            let day3Wind = document.querySelector("#day3-wind")
+                            let day3Humidity = document.querySelector("#day3-humidity")
+                            day3dt.textContent=data.list[24].dt_txt
+                            day3Temp.textContent = data.list[24].main.temp
+                            day3Wind.textContent = data.list[24].wind.speed
+                            day3Humidity.textContent= data.list[24].main.humidity
+                            let day4dt=document.querySelector("#col-4")
+                            let day4Temp = document.querySelector("#day4-temp")
+                            let day4Wind = document.querySelector("#day4-wind")
+                            let day4Humidity = document.querySelector("#day4-humidity")
+                            day4dt.textContent=data.list[32].dt_txt
+                            day4Temp.textContent = data.list[32].main.temp
+                            day4Wind.textContent = data.list[32].wind.speed
+                            day4Humidity.textContent= data.list[32].main.humidity
                         })
 
 
 
                         });
                 };
+
